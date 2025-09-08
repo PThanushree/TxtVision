89c2f3137791e09ca9e3d60a41dbdd28ef74082b
ThanushreeP <thanu22052005@gmail.com>
2025-09-08 08:35:17 +0530

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
   plugins: [
-    tailwindcss(),
+    react(),       // ✅ This enables automatic React JSX transform
+    tailwindcss(), // Your Tailwind setup
   ],
 })
